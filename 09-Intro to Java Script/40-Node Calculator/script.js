4a819922d740deacf1ed72ec1a90acb520ef6e0b
Leandro Heredia Martinez <lheredia8606@gmail.com>
2023-12-23 09:54:07 -0500

    Changes made to 09-40-calculator

--- 09-Intro to Java Script/40-Node Calculator/script.js
+++ 09-Intro to Java Script/40-Node Calculator/script.js
@@ -1,32 +1,36 @@
 const rs = require('readline-sync');
-console.log('Welcome to calculator');
-let flag = true;
-while(flag){
-  let operation = rs.question('What operation do you want to perform?');
-  while (!validateOperation(operation)){
-    operation = rs.question('Operation not supported, Please enter +, *, - or /');
-  }
-  num1 = rs.questionFloat("Enter the first number ");
-  num2 = rs.questionFloat("Enter the second number ");
-  while(num2 === 0 && operation === '/'){
-    num2 = rs.questionFloat("Division by 0 not allowed, Please enter another number");
-  }
-  console.log(`the operation ${num1} ${operation} ${num2} is equal to ${performOp(operation, num1, num2).toFixed(2)}`);
-  flag = rs.keyInYN(`Would you like to perform another?`);
+
+const objOperators = {
+  '+' : (a,b) => a + b,
+  '-' : (a,b) => a - b,
+  '*' : (a,b) => a * b,
+  '/' : (a,b) => a / b,
+}
+const operArr = Object.keys(objOperators);
+
+let operator, num1, num2 = '';
+
+const getOperator = (limit) => {
+  return rs.question('Please, Enter the operation you want to perform', {
+    limit,
+     limitMessage: `Operation not supported, choose one of this ( ${limit.join(", ")} )`,
+    });
 }
 
-function validateOperation (operation){
-  return (operation === '+' || operation === '-' || operation === '*' || operation === '/')
+const getNumbers = (op, str) =>{
+ let number = rs.questionInt(`Please enter the ${str} number`);
+ while(op === '/' && str === 'second' && number === 0){
+   console.log(`Division per 0 not allowed, Please another number`);
+   number = rs.questionInt(`Please enter the ${str}`);
+ }
+ return number
 }
 
-function performOp (op, n1, n2){
-  if(op === '+'){
-    return n1 + n2;
-  } else if (op === '-'){
-    return n1 - n2;
-  } else if (op === '*'){
-    return n1 * n2;
-  } else {
-    return n1 / n2;
-  }
+const doOperation  = () =>{
+  operator = getOperator(operArr);
+  num1 = getNumbers(operator, 'first');
+  num2 = getNumbers(operator, 'second');
+  return objOperators[operator](num1,num2);
 }
+
+console.log(doOperation());
